b774efc5637195b1808aaf546fb4926ba860da59
Afan-04 <abdulhaiafan@gmail.com>
2026-05-29 03:51:02 +0530

    done changes
    done

--- Weather App/script.js
+++ Weather App/script.js
@@ -6,6 +6,8 @@ const city = document.getElementById("city");
 const bottom = document.querySelector(".bottom");
 const subtitle = document.querySelector(".subtitle");
 const cityInput = document.getElementById("city");
+const mainTemp = document.querySelector(".main-temp");
+
 
 cityInput.addEventListener("keydown", (event) => {
   if (event.key === "Enter") {
@@ -36,7 +38,10 @@ btn.addEventListener("click", () => {
       const feelsLikeC = (data.main.feels_like - 273.15).toFixed(1);
 
       subtitle.textContent = data.name;
-
+      mainTemp.textContent = `${tempC}°C`;
+      mainTemp.style.animation = "none";
+      mainTemp.offsetHeight;
+      mainTemp.style.animation = "tempFade 0.6s ease";
       bottom.querySelector(".temp").innerHTML =
         `<img src="weather.png" class="w-icon">${tempC}°C`;
 
@@ -47,4 +52,37 @@ btn.addEventListener("click", () => {
         `<img src="feels-like.png" class="feels-like-icon">Feels ${feelsLikeC}°C`;
     })
     .catch(err => console.error("Fetch error:", err));
+});
+// dark mode
+const themeBtn = document.getElementById("theme-btn");
+
+/* LOAD SAVED THEME */
+
+if (localStorage.getItem("theme") === "dark") {
+
+  document.body.classList.add("dark-mode");
+  themeBtn.textContent = "☀️";
+
+}
+
+/* TOGGLE THEME */
+
+themeBtn.addEventListener("click", () => {
+
+  document.body.classList.toggle("dark-mode");
+
+  if (document.body.classList.contains("dark-mode")) {
+
+    themeBtn.textContent = "☀️";
+
+    localStorage.setItem("theme", "dark");
+
+  } else {
+
+    themeBtn.textContent = "🌙";
+
+    localStorage.setItem("theme", "light");
+
+  }
+
 });
